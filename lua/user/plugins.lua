6ace6a0d942544263b920040ce4559a54d2ff948
Haisaru <jasonli2442@gmail.com>
2023-07-13 08:05:55 -0400

    add autopairs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -103,6 +103,9 @@ return packer.startup(function(use)
 	use("rcarriga/nvim-dap-ui")
 	use("ravenxrz/DAPInstall.nvim")
 
+  -- autoclose
+  use "windwp/nvim-autopairs"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
