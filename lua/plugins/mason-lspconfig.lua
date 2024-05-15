feb4ae640aceb50ef8c00736124253b0eb8a3f68
Jason <jasonli2442@gmail.com>
2024-05-14 23:01:10 -0400

    get cland working

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -73,6 +73,25 @@ return {
 							end,
 						})
 					end,
+					["clangd"] = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.clangd.setup({
+							capabilities = { offsetEncoding = { "utf-16" } },
+							cmd = { "clangd", "--offset-encoding=utf-16" },
+							on_attach = function(client, bufnr)
+								if client.supports_method("textDocument/formatting") then
+									vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+									vim.api.nvim_create_autocmd("BufWritePre", {
+										group = augroup,
+										buffer = bufnr,
+										callback = function()
+											vim.lsp.buf.format()
+										end,
+									})
+								end
+							end,
+						})
+					end,
 				},
 			})
 		end,
